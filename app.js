66d01e22572effeabcabfa0c35b0b1add6db3833
Panisara Waisurasingh <Panisara.wai@gmail.com>
2021-04-19 21:35:54 +0700

    test noti for me pls idk the tokens
    query needed: noti_token

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const http = require('http').createServer(app);
 const bodyParser = require('body-parser');
 const { Expo } = require('expo-server-sdk');
 const mysql = require('mysql2');
-const expo = new Expo();
+const { expo } = new Expo();
+let expo = new Expo({ accessToken: process.env.EXPO_ACCESS_TOKEN });
 const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
@@ -174,16 +175,44 @@ app.post('/registeritem',upload.single('image'),  (req,res) =>{ // upload pictur
 });
 
 app.get('/noti', (req, res) => {
+    // const chunks = expo.chunkPushNotifications([
+    //     { to: "ExponentPushToken["+req.query.noti_token+"]", sound: "default", body: req.query.msg }
+    // ]);
 
-    const chunks = expo.chunkPushNotifications([
-        { to: "ExponentPushToken["+req.query.noti_token+"]", sound: "default", body: req.query.msg }
-    ]);
+    // for (let each of chunks) {
+    //     expo.sendPushNotificationsAsync(each)
+    // }
+    // res.send("Done");
 
-    for (let each of chunks) {
-        expo.sendPushNotificationsAsync(each)
-    }
-
-    res.send("Done");
+    let messages = [{
+        to : "ExponentPushToken["+req.query.noti_token+"]",
+        sound: "default",
+        title: "this should be the title",
+        body: "this is the small text right?",
+        data : {
+            msg: "idk why we are going so deep in JSON format i'm dizzy"
+        }
+    }];
+    let chunks = expo.chunkPushNotifications(messages);
+    let tickets = [];
+    (async () => {
+        // Send the chunks to the Expo push notification service. There are
+        // different strategies you could use. A simple one is to send one chunk at a
+        // time, which nicely spreads the load out over time:
+        for (let chunk of chunks) {
+          try {
+            let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+            console.log(ticketChunk);
+            tickets.push(...ticketChunk);
+            // NOTE: If a ticket contains an error code in ticket.details.error, you
+            // must handle it appropriately. The error codes are listed in the Expo
+            // documentation:
+            // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+          } catch (error) {
+            console.error(error);
+          }
+        }
+    })();
 });
 
 app.post('/createuser', (req, res) => {
