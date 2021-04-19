a86148b64026acb4c8c6d40da8941544fcb664d6
Panisara Waisurasingh <Panisara.wai@gmail.com>
2021-04-19 14:57:05 +0700

    itemreg json change
    โค้ดเก่ามันส่ง results[0] มันเลยขึ้น item เดียว ไม่หลายชิ้น

--- app.js
+++ app.js
@@ -264,7 +264,7 @@ app.get('/item_reg', (req, res) => {
     connection.query("SELECT (JSON_OBJECT('name', Items_"+type+".item_name, 'category', Items_"+type+".category, 'item_id', Items_"+type+".item_id, 'latitude', Items_"+type+".location_lat, 'longtitude', Items_"+type+".location_long, 'location', Items_"+type+".location_desc, 'description', Items_"+type+".description,'url', Items_"+type+".image_url, 'color', Items_"+type+"_color.color)) AS 'Registered' FROM Items_"+type+", Items_"+type+"_color WHERE type = 0 AND device_token != '" + req.query.token + "'", function (err, results) {
         if (err) throw err;
         console.log(results);
-        res.json(results[0]);
+        res.json(results);
     });
 });
 
@@ -273,7 +273,7 @@ app.get('/item_claimed', (req, res) => {
     connection.query("SELECT JSON_ARRAYAGG(JSON_OBJECT('name', item_name, 'item_id', item_id, 'location', location_desc,  'description', description, 'image', image_url))  AS 'Claimed' FROM Items_found WHERE type = 2", function (err, results) {
         if (err) throw err;
         console.log(results);
-        res.json(results[0]);
+        res.json(results);
     });
 });
 
